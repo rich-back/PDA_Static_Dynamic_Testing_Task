84c0fec9156afea984e55c4f1914a8877b7d6f0e
Richard Backhouse <114738026+rich-back@users.noreply.github.com>
2023-01-07 15:33:07 +0000

    final code submit

---
+++ part_2_code/specs/card_game_tests.py
@@ -0,0 +1,24 @@
+import unittest
+from src.card import Card
+from src.card_game import CardGame
+
+class TestCardGame(unittest.TestCase):
+
+    def setUp(self):
+        self.card1 = Card("Hearts", 10)
+        self.card2 = Card("Spades", 1)
+        self.cards = [self.card1, self.card2]
+
+        self.card_game = CardGame()
+
+    
+    def test_check_for_ace(self):
+        self.assertEqual(False, self.card_game.check_for_ace(self.card1))
+        self.assertEqual(True, self.card_game.check_for_ace(self.card2))
+
+    def test_highest_card(self):
+        self.assertEqual(self.card1, self.card_game.highest_card(self.card1, self.card2))
+
+    def test_cards_total(self):
+        self.assertEqual("You have a total of 11", self.card_game.cards_total(self.cards))
+        
